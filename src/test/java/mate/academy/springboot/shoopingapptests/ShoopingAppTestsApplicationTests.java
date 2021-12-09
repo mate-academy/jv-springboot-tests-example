a7ae3eea2cecd9f6c2f910f717061382bec7f612
Bohdan Chupika <chupika.bohdan@ukr.net>
2021-12-09 20:32:35 +0200

    add default SpringBoot test class

---
+++ src/test/java/mate/academy/springboot/shoopingapptests/ShoopingAppTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.springboot.shoopingapptests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoopingAppTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
