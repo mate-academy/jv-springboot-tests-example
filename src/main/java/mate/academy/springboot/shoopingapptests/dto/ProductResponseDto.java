94840eaa3d9846582333a0d09820c24320f5dfe1
Bohdan Chupika <chupika.bohdan@ukr.net>
2021-12-09 20:15:04 +0200

    add controller, service, repository, DTOs and mapper for Product entity

---
+++ src/main/java/mate/academy/springboot/shoopingapptests/dto/ProductResponseDto.java
@@ -0,0 +1,15 @@
+package mate.academy.springboot.shoopingapptests.dto;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponseDto {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+}
---
+++ src/main/java/mate/academy/springboot/shoopingapptests/service/ProductServiceImpl.java
@@ -0,0 +1,31 @@
+package mate.academy.springboot.shoopingapptests.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.springboot.shoopingapptests.model.Product;
+import mate.academy.springboot.shoopingapptests.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to) {
+        return productRepository.findAllByPriceBetween(from, to);
+    }
+}
